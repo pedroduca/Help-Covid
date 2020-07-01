6581f2241590539809efb1d71d21d6bded6dc70a
Pedro <testandoduca@gmail.com>
2020-06-30 21:56:25 -0300

    Corrigindo erros na API e fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import Home from './src/pages/Home';
 import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
